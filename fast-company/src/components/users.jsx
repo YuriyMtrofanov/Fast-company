23205ce29248ef62fe1b5f35bd794c0bafa55785
YuriyMitrofanov <yurasell@yandex.ru>
2022-12-25 18:10:22 +0300

    Add header function, bugs are fixed

--- fast-company/src/components/users.jsx
+++ fast-company/src/components/users.jsx
@@ -5,76 +5,92 @@ import "bootstrap/dist/css/bootstrap.css"
 export const Users = () => {
 
     const [usersAll, setUsersAll] = useState(api.users.fetchAll())
-    // 1. Требуется разобраться с уникальными ключами элементов
-    // 2. Подготовить функцию, которая будет корректно склонять слова
+    
+    const createHeader = () => {
 
-    const getQualityClasses = (array) => {
-        let classes = 'badge m-1 badge bg-'
-        classes += array.color
-        return classes
-    }
-
-    const getQualities = (array) => {
-        return array.map(quality => {
-            return (
-                <span 
-                    id = { quality._id }
-                    key = { quality._id }   // Данный атрибут не присваивается
-                    className = { getQualityClasses(quality) }
-                >
-                    { quality.name }
-                </span>
+        if (usersAll.length > 4) return (
+            <span className="badge bg-primary"> { usersAll.length } человек тусанет с тобой сегодня </span>
             )
-        })
+        else if (usersAll.length <= 4 && usersAll.length > 1) return (
+            <span className="badge bg-primary"> { usersAll.length } человека тусанут с тобой сегодня </span>
+            )
+        else if (usersAll.length === 1) return (
+            <span className="badge bg-primary"> { usersAll.length } человек тусанет с тобой сегодня </span>
+            )
+        else return (
+            <span className="badge bg-danger"> Никто с тобой не тусанет </span>
+            )
     }
 
-    const createLine = () => {
-        
-        return usersAll.map((line)=>{
+    const createThead = () => {
+        if (usersAll.length !== 0) return (
+             <tr>
+                 <th scope="col">Имя</th>
+                 <th scope="col">Качества</th>
+                 <th scope="col">Профессия</th>
+                 <th scope="col">Встретился, раз</th>
+                 <th scope="col">Оценка</th>
+                 <th scope="col"></th>
+             </tr>
+    )}
 
-            // Здесья я отлавливаю событие на кнопке "delete" получаю id элемента и с помощью setUsersAll() и метода filter() оставляю 
-            // в массиве с  юзерами только те значения, по которым не было кликов.
+    const createTbody = () => {
+
+        const getQualityClasses = (array) => {
+            let classes = 'badge m-1 badge bg-'
+            classes += array.color
+            return classes
+        }
+    
+        const getQualities = (array) => {
+            return array.map(quality => {
+                return (
+                    <span
+                        key = { quality._id }
+                        className = { getQualityClasses(quality) }
+                    >
+                        { quality.name }
+                    </span>
+                )
+            })
+        }
+        
+        return usersAll.map((person)=>{
+
             const handleDeleteElement = () => {
 
-                    // В принципе, эта часть кода не нужна. Она лишь позволяет получить элемент, по которому произошел клик
-                        const foundIndex = usersAll.findIndex((element) => {
-                            return element._id === line._id
-                        })
-                        const foundElement = usersAll.find((element) => {
-                            return element._id === line._id
-                        })
+                // В результате вызова данного метода мы сохраняем в массиве только те элементы, которые
+                // не равны "person._id" (то есть не равны id полученного элемента при нажатии на кнопку)
+                setUsersAll(prevState => prevState.filter((element) => element._id !== person._id))
 
-
-                    console.log(foundIndex, foundElement)
-                
-                // В результате вызова данного метода мы сохраняем в массиве только те элементы, которые не 
-                // равны "line._id" (то есть не равны id полученного элемента при нажатии на кнопку)
-                setUsersAll(prevState => prevState.filter((element) => element._id !== line._id))
+                    //     const foundIndex = usersAll.findIndex((element) => {
+                    //         return element._id === person._id
+                    //     })
+                    //     const foundElement = usersAll.find((element) => {
+                    //         return element._id === person._id
+                    //     })
+                    // console.log(foundIndex, foundElement)
             }
 
             return (
-                <>  
-                    <tr id = { line._id }
-                        key = { line._id }  // Данный атрибут не присваивается
-                    >    
-                            <td>{ line.name }</td>
+                    <tr key = { person._id }>
+                            <td>{ person.name }</td>
                             <td> 
-                                <h6> { getQualities(line.qualities) }</h6>
+                                <h6> { getQualities(person.qualities) }</h6>
                             </td>
-                            <td>{ line.profession.name }</td>
-                            <td>{ line.completedMeetings }</td>
-                            <td>{ line.rate }/5</td>
+                            <td>{ person.profession.name }</td>
+                            <td>{ person.completedMeetings }</td>
+                            <td>{ person.rate }/5</td>
                             <td>
                                 <button 
                                     type="button" 
                                     className="btn btn-danger" 
-                                    onClick = { () => handleDeleteElement(line._id) }
+                                    onClick = { () => handleDeleteElement(person._id) }
                                 >
                                         Delete
                                 </button>
                         </td>
                     </tr>
-                </>
             )
         })
     }
@@ -83,22 +99,16 @@ export const Users = () => {
 
         return (
             <>  
-                <h2> 
-                    <span className="badge bg-primary"> { usersAll.length } человек тусанет с тобой сегодня </span>
-                </h2>
+                <h1>
+                    { createHeader() }
+                </h1>
+                    
                 <table className="table table-striped">
                     <thead>
-                        <tr>
-                            <th scope="col">Имя</th>
-                            <th scope="col">Качества</th>
-                            <th scope="col">Профессия</th>
-                            <th scope="col">Встретился, раз</th>
-                            <th scope="col">Оценка</th>
-                            <th scope="col"></th>
-                        </tr>
+                        { createThead() }
                     </thead>
                     <tbody>
-                        { createLine() }
+                        { createTbody() }
                     </tbody>
                 </table>
             </>
--- fast-company/src/index.js
+++ fast-company/src/index.js
@@ -7,10 +7,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    // <Users/>
+  <React.StrictMode>
     <Users/>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 reportWebVitals();
