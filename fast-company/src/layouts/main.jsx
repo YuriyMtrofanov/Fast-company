8e59314125dcec4b9411c4e47e43e297c5b9f173
YuriyMitrofanov <yurasell@yandex.ru>
2023-03-22 21:19:53 +0300

    Initialized Firebase and mocData in project

--- fast-company/src/layouts/main.jsx
+++ fast-company/src/layouts/main.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import useMocData from "../utils/mocData";
 
 const Main = () => {
+    const { error, initialize, progress, status } = useMocData();
+
+    const handleClick = () => {
+        initialize();
+    };
     return (
-        <h1>Main Page</h1>
+        <div className="containe mt-5">
+            <h1>Main Page</h1>
+            <ul>
+                <li>Status: {status}</li>
+                <li>Progress: {progress}%</li>
+                {error && <li>Error: {error}</li>}
+            </ul>
+            <h3>Инициализация данных в FireBase</h3>
+            <button className="btn btn-primary" onClick = {handleClick}>Инициализировать</button>
+        </div>
     );
 };
 
--- fast-company/src/services/http.service.js
+++ fast-company/src/services/http.service.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { toast } from "react-toastify"; // Импортируем метод для отображения ошибок для пользователя
-import config from "../config.json";
+// import { toast } from "react-toastify"; // Импортируем метод для отображения ошибок для пользователя
+import configFile from "../config.json";
 // Зададим базовый роут ссылки на сервер (http://localhost:4000/api/v1/),
 // Т.о. мы можем при вызове URL вообще не указывать знвчение этой ссылки.
 // Она как бы подтягивается по умолчанию.
-axios.defaults.baseURL = config.apiEndpoint;
+axios.defaults.baseURL = configFile.apiEndpoint;
 
 // Для обработки неожидаемых ошибок используем метод "interceptors" (перехватчи)
 // Данный метод может перехватывать как request (запрос), так и response (ответ от сервера)
@@ -12,7 +12,7 @@ axios.defaults.baseURL = config.apiEndpoint;
 // Для удачного просто выведем коллбэк (response) => response т.е. ответ от сервера без изменений,
 // а для неудачного - поймаем ошибку. В прортивном случае передаем ошиобку дальше и продолжаем
 // работу промиса т.к. мы не обрабатываем иные ошибки в этом обработчике
-axios.interceptors.response.use(
+/* axios.interceptors.response.use(
     (response) => response,
     function (error) {
         const expectedErrors = error.response && error.response.status >= 400 && error.response.status < 500;
@@ -25,6 +25,39 @@ axios.interceptors.response.use(
             toast.error("Something goes wrong. Try later.");
             // toast("unexpected error");
         }
+        return Promise.reject(error);
+    }
+); */
+
+// Так как от fireBase мы получаем данные в виде объекта, но нам нужен массив, то требуется
+// преобразовать данные с помощью функции:
+function transformData(data) {
+    return data
+        ? Object.keys(data).map(key => ({ ...data[key] }))
+        : [];
+};
+// Далее при обоработке ответа мы вызываем transformData() и получаем массив с данными
+axios.interceptors.response.use(
+    (response) => {
+        response.data = { content: transformData(response.data) };
+        // console.log("response.data.content", response.data);
+        return response;
+    }
+);
+
+axios.interceptors.request.use(
+    function (config) {
+        if (configFile.isFireBase) {
+            const isContainsSlash = /\/$/gi.test(config.url);
+            config.url = (
+                isContainsSlash
+                    ? config.url.slice(0, -1)
+                    : config.url) + ".json";
+            // console.log("config.url", config.url); // quality.json или profession.json
+        }
+        return config;
+    },
+    function (error) {
         return Promise.reject(error);
     }
 );
